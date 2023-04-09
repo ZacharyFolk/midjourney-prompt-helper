d512216012269f64baa58d9c0cdef1de01c24bf3
ZacharyFolk <zacharyfolk@gmail.com>
2023-04-08 17:06:24 -0700

    :camera_flash: Fixing up image feed from Reddit, good enough for now...

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import {
   Paper,
   Grid,
   Container,
+  ImageList,
+  ImageListItem,
+  ImageListItemBar,
   Typography,
   TextField,
   Button,
@@ -34,30 +37,75 @@ const StyledPaper = styled(Paper)(({ theme }) => ({
   textAlign: 'center',
   color: theme.palette.text.secondary,
 }));
+const StyledImg = styled('img')({
+  maxWidth: '100%',
+});
 
+function srcset(image, size, rows = 1, cols = 1) {
+  return {
+    src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
+    srcSet: `${image}?w=${size * cols}&h=${
+      size * rows
+    }&fit=crop&auto=format&dpr=2 2x`,
+  };
+}
 function RedditImageScraper({ subreddit }) {
   const [images, setImages] = useState([]);
+  const [sort, setSort] = useState('new'); // top, hot, new, rising, controversial - with top, controversial you can add t=hour, day, week, month, year, all
 
   useEffect(() => {
     async function fetchImages() {
       const response = await fetch(
-        `https://www.reddit.com/r/${subreddit}/new.json?limit=10`
+        `https://www.reddit.com/r/${subreddit}/new.json?sort=${sort}&t=all&limit=100`
       );
       const data = await response.json();
+      console.log(data.data);
       const newImages = data.data.children
         .filter((post) => post.data.post_hint === 'image')
-        .map((post) => post.data.url);
+        .map((post) => {
+          let rows, cols;
+          if (Math.random() < 0.2) {
+            // 25% chance or item getting a row or col attribute for quilted
+            rows = Math.floor(Math.random() * 2) + 1;
+            cols = Math.random() < 0.5 ? 1 : 2; // 50% chance of 1 col, 50% chance of 2 cols
+          } else {
+            rows = Math.random() < 0.5 ? 1 : 2; // 50% chance of 1 row, 50% chance of 2 rows
+            cols = Math.floor(Math.random() * 2) + 1;
+          }
+          return {
+            img: post.data.url,
+            title: post.data.title,
+            ...(rows && { rows }),
+            ...(cols && { cols }),
+          };
+        });
+      console.log(newImages);
       setImages(newImages);
     }
     fetchImages();
   }, [subreddit]);
 
   return (
-    <div>
-      {images.map((image) => (
-        <img key={image} src={image} alt='' />
+    <ImageList
+      sx={{ width: '100%', height: 450 }}
+      cols={4}
+      variant='quilted'
+      rowHeight={121}
+    >
+      {images.map((item) => (
+        <ImageListItem
+          key={item.img}
+          cols={item.cols || 1}
+          rows={item.rows || 1}
+        >
+          <img
+            {...srcset(item.img, 121, item.rows, item.cols)}
+            alt={item.title}
+            loading='lazy'
+          />
+        </ImageListItem>
       ))}
-    </div>
+    </ImageList>
   );
 }
 
@@ -77,9 +125,7 @@ function App() {
               </Button>
             </Grid>
           </Grid>
-          <Grid>
-            <RedditImageScraper subreddit='midjourney' />
-          </Grid>
+          <Grid></Grid>
           <Box mt={4}>
             <Grid container spacing={10} alignItems='center'>
               <Grid item xs={12}>
@@ -110,6 +156,9 @@ function App() {
                 </Grid>
               </Grid>
               <Grid item xs={6}>
+                <Grid item xs={12}>
+                  <RedditImageScraper subreddit='midjourney' />
+                </Grid>
                 <Grid item xs={12}>
                   <Typography variant='h5' component='h2'>
                     Sliders
