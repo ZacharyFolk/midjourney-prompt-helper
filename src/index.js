62af25fc882657b0c5c8f04276b1c13004fee390
ZacharyFolk <zacharyfolk@gmail.com>
2023-04-14 10:19:03 -0700

    disable the logging

--- src/index.js
+++ src/index.js
@@ -19,16 +19,16 @@ root.render(
   </React.StrictMode>
 );
 
-//  log the memory usage in MB every ten seconds
-setInterval(() => {
-  const { usedJSHeapSize, jsHeapSizeLimit } = window.performance.memory;
-  const usedMemory = Math.round(usedJSHeapSize / (1024 * 1024));
-  const totalMemory = Math.round(jsHeapSizeLimit / (1024 * 1024));
-  const memoryUsage = `${usedMemory} MB out of ${totalMemory} MB`;
-  console.log(`Memory usage: ${memoryUsage}`);
-}, 10000);
+// //  log the memory usage in MB every ten seconds
+// setInterval(() => {
+//   const { usedJSHeapSize, jsHeapSizeLimit } = window.performance.memory;
+//   const usedMemory = Math.round(usedJSHeapSize / (1024 * 1024));
+//   const totalMemory = Math.round(jsHeapSizeLimit / (1024 * 1024));
+//   const memoryUsage = `${usedMemory} MB out of ${totalMemory} MB`;
+//   console.log(`Memory usage: ${memoryUsage}`);
+// }, 10000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
+// reportWebVitals(console.log);
